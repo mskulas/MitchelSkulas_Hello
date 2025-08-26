41d210b307a58573b5652d1015b2817c5ae43053
mskulas <mskulas@murraystate.edu>
2025-08-25 19:27:41 -0500

    created JavaDoc for HelloWorld and generated HTML

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,46 +1,52 @@
+/**
+ * The HelloWorld class represents a person with a name and optionally an age,
+ * and provides methods to introduce and greet them.
+ */
 public class HelloWorld {
 
     String name;
     private int age;
 
+    /**
+     * Constructs a HelloWorld object with the specified name.
+     *
+     * @param name the name of the person
+     */
     public HelloWorld(String name) {
-
         this.name = name;
-
     }
 
+    /**
+     * Constructs a HelloWorld object with the specified name and age.
+     *
+     * @param name the name of the person
+     * @param age the age of the person (optional)
+     */
     public HelloWorld(String name, int age) {
-
         this.name = name;
         this.age = age;
-
     }
 
+    /**
+     * Introduces the person by printing their name and age.
+     */
     public void introduce() {
-
         System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
-
     }
 
+    /**
+     * Greets the person using their name.
+     */
     public void greet() {
-
         System.out.println("Hello, " + name + "!");
-
     }
 
+    /**
+     * Greets another person by name.
+     *
+     * @param name the name of the person to greet
+     */
     public void greet(String name) {
-
         System.out.println("Hello, " + name + "!");
-
     }
-    
-    public static void main(String[] args) {
-
-    HelloWorld student = new HelloWorld("Mitchel", 19);
-    student.introduce();
-    student.greet();
-    student.greet("Not Mitchel");
-
-    }
-
 }
